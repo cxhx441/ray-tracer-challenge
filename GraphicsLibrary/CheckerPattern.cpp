5cf23a0940fc2ae622f2de0496735ac6aaa8d1b1
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-08-30 21:16:13 -0700

    apply epsilon to pattern_color_at to remove acne from images.

--- GraphicsLibrary/CheckerPattern.cpp
+++ GraphicsLibrary/CheckerPattern.cpp
@@ -8,9 +8,10 @@
 CheckerPattern::CheckerPattern(Tuple color_a, Tuple color_b) : color_a(color_a), color_b(color_b) {}
 
 Tuple CheckerPattern::pattern_color_at(const Tuple &pattern_point) const {
-    int x = floor(pattern_point.x);
-    int y = floor(pattern_point.y);
-    int z = floor(pattern_point.z);
+    float epsilon = 1e-5; // to get rid of "acne".
+    int x = floor(pattern_point.x + epsilon);
+    int y = floor(pattern_point.y + epsilon);
+    int z = floor(pattern_point.z + epsilon);
     if ( (x + y + z) % 2 == 0 )
         return color_a;
     return color_b;
